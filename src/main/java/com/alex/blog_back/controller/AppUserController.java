c2a2095c83890a59334d1306b586986fc7afd52f
ablache <alexandreblache.pro@gmail.com>
2021-11-17 10:35:39 +0100

    cors

--- src/main/java/com/alex/blog_back/controller/AppUserController.java
+++ src/main/java/com/alex/blog_back/controller/AppUserController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@CrossOrigin
 public class AppUserController {
     private final AppUserService appUserService;
 
--- src/main/java/com/alex/blog_back/security/SecurityConfig.java
+++ src/main/java/com/alex/blog_back/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilter(new JwtAuthFilter(authenticationManager()))
